d677317acc565aec571e27d5c41ddca06c5b6195
Midlaj M <midumidlaj581@gmail.com>
2026-02-26 14:20:57 +0530

    Fix CSP blocking inline handlers by disabling Helmet CSP

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ const io = new Server(server, {
 });
 
 app.set('trust proxy', 1);
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
 app.use(cors(createCorsOptions()));
 app.use(apiLimiter);
 app.use(express.json({ limit: '15mb' }));
